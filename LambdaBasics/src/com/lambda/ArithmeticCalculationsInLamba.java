b2e45b00582838a8847a045e4daa34d3259ba0f4
alagu031325 <alagu031325@gmail.com>
2022-09-21 11:19:33 +0100

    Revised code

--- LambdaBasics/src/com/lambda/ArithmeticCalculationsInLamba.java
+++ LambdaBasics/src/com/lambda/ArithmeticCalculationsInLamba.java
@@ -33,52 +33,50 @@ public class ArithmeticCalculationsInLamba {
 			System.out.println("Code is optimized for integer values");
 			System.out.println("Enter first number");
 			number1 = readValue(scannerInstance);
+			if (number1 != 0) {
+				System.out.println("Enter second number");
+				number2 = readValue(scannerInstance);
 
-			System.out.println("Enter second number");
-			number2 = readValue(scannerInstance);
+				if (number2 != 0) {
 
-			// Lambda expression
-			switch (operator) {
-			case '+':
-				result = printArithmeticResults(number1, number2,
-						(lamdaValue1, lamdaValue2) -> lamdaValue1 + lamdaValue2);
-				System.out.println("The sum of two numbers is " + result);
-				break;
+					// Lambda expression
+					switch (operator) {
+					case '+':
+						result = printArithmeticResults(number1, number2,
+								(lamdaValue1, lamdaValue2) -> lamdaValue1 + lamdaValue2);
+						System.out.println("The sum of two numbers is " + result);
+						break;
 
-			case '-':
-				result = printArithmeticResults(number1, number2,
-						(lamdaValue1, lamdaValue2) -> lamdaValue1 - lamdaValue2);
-				System.out.println("The subtracted value is " + result);
-				break;
+					case '-':
+						result = printArithmeticResults(number1, number2,
+								(lamdaValue1, lamdaValue2) -> lamdaValue1 - lamdaValue2);
+						System.out.println("The subtracted value is " + result);
+						break;
 
-			case '*':
-				result = printArithmeticResults(number1, number2,
-						(lamdaValue1, lamdaValue2) -> lamdaValue1 * lamdaValue2);
-				System.out.println("The multiplied value is " + result);
-				break;
+					case '*':
+						result = printArithmeticResults(number1, number2,
+								(lamdaValue1, lamdaValue2) -> lamdaValue1 * lamdaValue2);
+						System.out.println("The multiplied value is " + result);
+						break;
 
-			case '/':
-				System.out.println("This is a safe divide");
-				result = printArithmeticResults(number1, number2, (lamdaValue1, lamdaValue2) -> {
-					try {
-						return lamdaValue1 / lamdaValue2;
-					} catch (ArithmeticException e) {
-						System.out.println("Enter valid input");
-						return 0;
+					case '/':
+						result = printArithmeticResults(number1, number2,
+								(lamdaValue1, lamdaValue2) -> lamdaValue1 / lamdaValue2);
+						System.out.println("The divided value is " + result);
+						break;
+
+					default:
+						System.out.println("Enter a valid operator");
+						break;
 					}
-				});
-				if (result != 0)
-					System.out.println("The divided value is " + result);
-				break;
 
-			default:
-				System.out.println("Enter a valid option");
-				break;
+					System.out.println("Do you want to continue type y/n");
+					response = scannerInstance.next().charAt(0);
+
+				}
 			}
-			System.out.println("Do you want to continue type y/n");
-			response = scannerInstance.next().charAt(0);
 		} while (response == 'y');
-		System.out.println("Good bye");
+		System.out.println("See you soon");
 	}
 
 	private static int readValue(Scanner scannerInstance) {
---
+++ LambdaBasics/src/com/lambda/SortingElementsInLambda.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.lambda;
+
+/**
+ * @author alagu
+ *
+ */
+public class SortingElementsInLambda {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
